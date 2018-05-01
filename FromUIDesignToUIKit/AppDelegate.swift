3d7d7a361c973c215d4464cf9976417dd713a7be
vdka <ethanjackwitz@gmail.com>
2018-05-01 11:16:43 +1000

    Remove Main.storyboard & initialize ViewController programmatically

--- FromUIDesignToUIKit/AppDelegate.swift
+++ FromUIDesignToUIKit/AppDelegate.swift
@@ -4,9 +4,15 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+    var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        let vc = ViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        window!.rootViewController = nav
+        window!.makeKeyAndVisible()
+
         return true
     }
 }
--- FromUIDesignToUIKit/ViewController.swift
+++ FromUIDesignToUIKit/ViewController.swift
@@ -3,4 +3,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError(#function)
+    }
 }
